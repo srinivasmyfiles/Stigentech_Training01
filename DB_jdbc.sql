6adec2cf6d4fd11d61fc7248cdb50fd18c04b1da
srinivasmyfiles <kapasrinivas9@gmail.com>
2021-01-05 13:08:46 +0530

    Create DB_jdbc.sql

---
+++ DB_jdbc.sql
@@ -0,0 +1,12 @@
+create database test
+use test
+create table `Product` (
+   id INT  NOT NULL AUTO_INCREMENT,
+  name VARCHAR(20)  NOT NULL, 
+  price INT   NOT NULL ,
+  description VARCHAR(20)  NOT NULL,
+  PRIMARY KEY ( id)
+  )
+  
+
+  
